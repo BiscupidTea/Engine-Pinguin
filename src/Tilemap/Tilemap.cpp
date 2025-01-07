0c807926283cb2f57c01ff90d974a962945854b0
fekox <facusantos123@gmail.com>
2025-01-07 12:28:14 -0300

    fix tilemap

--- src/Tilemap/Tilemap.cpp
+++ src/Tilemap/Tilemap.cpp
@@ -1,1 +1,29 @@
 #include "Tilemap.h"
+
+Tilemap::Tilemap(int layers, int columns, int rows)
+{
+	for (int i = 0; i < layers; i++)
+	{
+		tilemap[i] = vector<vector<Tile>>(rows);
+
+		for (int j = 0; j < rows; j++)
+		{
+			tilemap[i][j] = vector<Tile>(columns);
+
+			for (int k = 0; k < columns; k++)
+			{
+				tilemap[i][j][k] = Tile();
+			}
+		}
+	}
+}
+
+Tilemap::~Tilemap()
+{
+
+}
+
+void Tilemap::AddTile(int layer, int column, int row, Tile& newTile)
+{
+	tilemap[layer][column][row] = newTile;
+}
--- src/Tilemap/Tilemap.h
+++ src/Tilemap/Tilemap.h
@@ -1,14 +1,26 @@
 #pragma once
 #include "Tools/dll-Exp-Inp.h"
+#include "Tile.h"
+#include "Entity/Sprite.h"
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
 
 class EXPORT Tilemap
 {
 private:
 
+	vector<vector<vector<Tile>>> tilemap;
+
+	int layers, columns, rows;
+
 public:
 
-	Tilemap();
+	Tilemap(int layers, int columns, int rows);
 	~Tilemap();
-
+	
+	void AddTile(int layer, int column, int row, Tile& newTile);
 };
 
